b6cbf9f3bd20eed93a4f83e77df50ae2faead00e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-06-02 23:02:16 -0700

    created a GET route to get all invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = new express.Router();
+const db = require('../db');
 
-
-
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await db.query(`SELECT id, comp_code FROM invoices`);
+        return res.json({invoices: result.rows});
+    }
+    catch(e) {
+        return next(e);
+    }
+})
 
 
 

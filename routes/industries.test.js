cb99fbc6cd0c9c25c2edb66569317a33655f8494
Jeff Hall <jeff.leon.hall@gmail.com>
2021-06-05 15:58:00 -0700

    added a file for testing the industries endpoints

--- routes/industries.test.js
+++ routes/industries.test.js
@@ -5,3 +5,40 @@ const app = require('../app');
 const db = require('../db');
 const request = require('supertest');
 
+let testIndustry;
+let testCompany;
+
+beforeEach(async () => {
+    const indResult = await db.query(`
+    INSERT INTO industries (code, industry)
+    VALUES ('aero', 'aeronautics')
+    RETURNING code, industry`);
+
+    const compResult = await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('bg', 'boing', 'An airplane company')
+    RETURNING code, name, description`);
+
+    await db.query(`
+    INSERT INTO companies_industries (comp_code, ind_code)
+    VALUES ('bg', 'aero')`);
+
+    testIndustry = indResult.rows[0];
+    testCompany = compResult.rows[0];
+});
+
+afterEach(async () => {
+    await db.query(`DELETE FROM industries`);
+    await db.query(`DELETE FROM companies`);
+    await db.query(`DELETE FROM companies_industries`);
+});
+
+afterAll(() => {
+    db.end();
+})
+
+describe(`GET /industries`, () => {
+    test('Get all industries w/ company codes', async () => {
+        expect(1).toBe(1);
+    })
+})

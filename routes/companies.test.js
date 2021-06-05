5183138f0b8e0bb1f82100232d804b65ff9c9189
Jeff Hall <jeff.leon.hall@gmail.com>
2021-06-04 20:15:41 -0700

    added supertest as a requirement and added boilerplate for the companies test file

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -4,3 +4,27 @@ const app = require('../app');
 const db = require('../db');
 const request = require('supertest');
 
+let testCompany;
+
+beforeEach(async () => {
+    const result = await db.query(`
+        INSERT INTO companies (code, name, description)
+        VALUES ('dd', 'DataDog', 'Cloud monitoring as a service')
+        RETURNING code, name, description`);
+    
+    testCompany = result.rows[0];
+})
+
+afterEach(async () => {
+    await db.query('DELETE FROM companies');
+})
+
+afterAll(() => {
+    db.end();
+})
+
+describe('GET /companies', () => {
+    test('Get a list of companies', () => {
+        expect(1).toEqual(1)
+    })
+})
